d2859f7b801126db6f17f118e17855f0c5c95dbf
AxelCHCR <axelchaves.r@estudiantec.cr>
2023-10-29 00:25:46 -0600

    feat: function cargarPreguntas added

--- proyecto.py
+++ proyecto.py
@@ -1,3 +1,12 @@
+import random as r
+def iniciarJuego(cantJugadores):
+    preguntas = cargarPreguntas("preguntas.dat")
+
+def cargarPreguntas(archivo):
+    archivo = open(archivo, "r")
+    preguntas = archivo.read()
+    archivo.close()
+    return eval(preguntas)
 def menu():
     print("""
         TRIVIAINFO
@@ -6,43 +15,16 @@ def menu():
         3. Estadísticas
         4. Salir
         """)
+    opcion = int(input("Ingrese una opción: "))
+    if opcion == 1:
+        jugadores = int(input("Ingrese la cantidad de jugadores: "))
+    elif opcion == 2:
+        pass
+    elif opcion == 3:
+        pass
+    elif opcion == 4:
+        pass
 
+#menu()
+print(cargarPreguntas())
 
-menu()
-{
-    [0]: [("¿Cuál es el equivalente binario del número decimal 128?\na. 10000000\n b.10000001\n. c. 10000010\n d. 10000011\n", "a"),
-          ("¿Cuál es el equivalente binario del número decimal 255?\na. 11111110\n b.11111111\n. c. 11111110\n d. 11111100\n", "b"),
-          ("¿Cuál es el equivalente binario del número decimal 100? \na. 1100100\n b.1100101\n. c. 1100110\n d. 1100111\n", "a"),
-          (" ¿Cuál es el equivalente binario del número decimal 200?\na. 11001000\n b.1100101\n. c. 1100110\n d. 1100111\n", "a"),
-          (" ¿Cuál es el equivalente binario del número decimal 350?\na. 1100100\n b.1100101\n. c. 1100110\n d. 101011110\n", "d")],
-    [1]: [("¿Cuál es el equivalente octal del número decimal 100?\na. 100\nb. 200\nc. 270\nd. 144", "d"),
-          (" ¿Cuál es el equivalente octal del número decimal 250?\na. 110\nb. 372\nc. 230\nd. 144", "b"),
-          (" ¿Cuál es el equivalente octal del número decimal 15? \na. 17\nb. 16\nc. 11\nd. 19", "a"),
-          (" ¿Cuál es el equivalente octal del número decimal 50?\na. 62\nb. 68\nc. 63\nd. 64", "a")
-          ],
-    [2]: [("¿Cuál es el equivalente hexadecimal del número decimal 100? \na. 64\nb. 32\nc. 100\nd. 64", "a"),
-          ("¿Cuál es el equivalente hexadecimal del número decimal 128? \na. C0\nb. 8000\nc. 100\nd. C000", "d"),
-          ("¿Cuál es el equivalente hexadecimal del número decimal 255? \na. FF\nb. 100\nc. 200\nd. 300", "a"),
-          ("¿Cuál es el equivalente hexadecimal del número decimal 350? \na. 15E\nb. 14F\nc. 14E\nd. 15F", "a"),
-          ("¿Cuál es el equivalente hexadecimal del número decimal 200? \na. 128\nb. 64\nc. C8\nd. 100", "c")
-          ],
-    [3]: [("¿Cuál es el equivalente octal del número binario 101000? \na. 50\nb. 40\nc. 60\nd. 70", "a"),
-          ("¿Cuál es el equivalente octal del número binario 110101? \na. 65\nb. 55\nc. 75\nd. 85", "a"),
-          (" ¿Cuál es el equivalente octal del número binario 111111? \na. 67\nb. 77\nc. 57\nd. 47", "b"),
-          ],
-    [4]: [("¿Cuál es el equivalente hexadecimal del número binario 101000? \na. 50\nb. 28\nc. 28\nd. 70", "b"),
-          ("¿Cuál es el equivalente hexadecimal del número binario 110101? \na. 65\nb. 55\nc. 35\nd. 85", "c"),
-          ("¿Cuál es el equivalente hexadecimal del número binario 11111111? \na. FF\nb. 77\nc. 57\nd. 47", "a"),
-          ],
-    [5]: [("¿Cuál es el equivalente decimal del número binario 101000? \na. 50\nb. 40\nc. 28\nd. 70", "b"),
-          ("¿Cuál es el equivalente decimal del número binario 110101? \na. 65\nb. 55\nc. 53\nd. 85", "c"),
-          ("¿Cuál es el equivalente decimal del número binario 111111? \na. 67\nb. 77\nc. 63\nd. 47", "c"),
-          ("¿Cuál es el equivalente decimal del número binario 111101? \na. 61\nb. 77\nc. 57\nd. 47", "a"),
-          ("¿Cuál es el equivalente decimal del número binario 110111? \na. 55\nb. 77\nc. 57\nd. 47", "a")],
-    [6]: [("¿Cuál es el resultado de la suma binaria de 1011 y 1101? \na. 11000\nb. 11010\nc. 11100\nd. 11110", "a"),
-          ("¿Cuál es el resultado de la multiplicación binaria de 1011 y 1101? \na. 11111\nb. 11101\nc. 11100\nd. 10001111", "d"),
-          ("¿Cuál es el resultado de la división binaria de 1011 por 1101? \na. 00\nb. 1\nc. 10\nd. 11", "a"),
-          ("¿Cuál es el resultado de la división binaria de 11101 por 11010? \na. 00\nb. 11\nc. 10\nd. 01", "d"),
-          ("¿Cuál es el resultado de la resta binaria de 11101 y 11010? \na. 00\nb. 11\nc. 10\nd. 01", "b"),
-          ]
-}
