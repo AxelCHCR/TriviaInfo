5894ec163235bca5cbccf7885f7760f2e080462c
AxelCHCR <axelchaves.r@estudiantec.cr>
2023-10-31 15:15:20 -0600

    hotfix: code formatted

--- proyecto.py
+++ proyecto.py
@@ -1,4 +1,6 @@
 import random as r
+
+
 def obtenerPreguntasPorCategoriaConRespuesta():
     preguntas = cargarDatos("preguntas.dat")
     for i in range(0, 7):
@@ -6,17 +8,22 @@ def obtenerPreguntasPorCategoriaConRespuesta():
         print(preguntas[i][pregunta][0])
         print("Respuesta: ", preguntas[i][pregunta][1])
 
+
 def obtenerPreguntaAleatoriaPorCategoria(categoria):
-    preguntas  = cargarDatos("preguntas.dat")
-    pregunta  = r.randint(0, len(preguntas[categoria])-1)
+    preguntas = cargarDatos("preguntas.dat")
+    pregunta = r.randint(0, len(preguntas[categoria])-1)
     print(preguntas[categoria][pregunta][0])
     print("Respuesta: ", preguntas[categoria][pregunta][1])
+
+
 def tablaPuntuaciones():
     datosAciertos = cargarDatos("juegosDeUsuarios.dat")
     datosFallos = cargarDatos("fallosPorJugador.dat")
     for i, j in datosAciertos.items():
         print(f"{i} acertó {j} preguntas y falló {datosFallos[i]}")
         print("Promedio: ", (j/(j+datosFallos[i]))*100, "%")
+
+
 def estadisticasPorJugador(jugador):
     datos = cargarDatos("juegosDeUsuarios.dat")
     datosVictorias = cargarDatos("ganadores.dat")
@@ -29,18 +36,25 @@ def estadisticasPorJugador(jugador):
         print(f"Cantidad de victorias: {datosVictorias[jugador]}")
     except:
         print(f"{jugador} aún no ha ganado ninguna partida.")
+
+
 def topJugadores():
     datos = cargarDatos("juegosDeUsuarios.dat")
     print("Los jugadores con más juegos son: ")
     datosOrdenados = sorted(datos.items(), key=lambda x: x[1], reverse=True)
     for i in range(0, len(datosOrdenados)):
-        print(f"{i+1}. {datosOrdenados[i][0]} Cantidad de juegos: {datosOrdenados[i][1]}")
+        print(
+            f"{i+1}. {datosOrdenados[i][0]} Cantidad de juegos: {datosOrdenados[i][1]}")
+
+
 def topPreguntasFalladas():
     datos = cargarDatos("preguntasFalladas.dat")
     print("Las preguntas más falladas son: ")
     datosOrdenados = sorted(datos.items(), key=lambda x: x[1], reverse=True)
-    for i in range(0, len(datosOrdenados)):   
-        print(f"{i+1}. {datosOrdenados[i][0]} Cantidad de fallos: {datosOrdenados[i][1]}")
+    for i in range(0, len(datosOrdenados)):
+        print(
+            f"{i+1}. {datosOrdenados[i][0]} Cantidad de fallos: {datosOrdenados[i][1]}")
+
 
 def addRegistroJugadasPorJugador(datos, nombre):
     try:
@@ -48,35 +62,45 @@ def addRegistroJugadasPorJugador(datos, nombre):
     except:
         datos[nombre] = 1
     return datos
+
+
 def addPreguntasFalladas(pregunta, datos):
     try:
         datos[pregunta] += 1
     except:
         datos[pregunta] = 1
     return datos
+
+
 def guardarPreguntaFallada(archivo, datos):
     archivo = open(archivo, "w")
     archivo.write(str(datos))
     archivo.close()
+
+
 def escribirDatos(archivo, datos):
     archivo = open(archivo, "w")
     archivo.write(str(datos))
     archivo.close()
+
+
 def sumarJuegoAJugador(jugador, datos):
-    try :
+    try:
         datos[jugador] += 1
     except:
         datos[jugador] = 1
     return datos
+
+
 def iniciarJuego():
     equipos = input("¿Hay equipos? (s/n): ").lower()
-    tipoJuego = None #True para juego en equipo, False para juego individual
+    tipoJuego = None  # True para juego en equipo, False para juego individual
     datosJugadores = []
     if equipos == "s":
         print("Para que el juego sea justo, ambos equipos serán de dos jugadores. ")
-        tipoJuego  = True
+        tipoJuego = True
         cantJugadores = 4
-        for i in range(1,3):
+        for i in range(1, 3):
             print(f"Datos para el equipo {i}. ")
             nombreEquipo = input("Ingrese el nombre del equipo: ").lower()
             jugador1 = input("Ingrese el nombre del primer jugador: ").lower()
@@ -103,7 +127,7 @@ def iniciarJuego():
             turno = 0
             preguntasPorJugador += 1
         while True:
-            seccion = r.randint(0,6)
+            seccion = r.randint(0, 6)
             pregunta = r.randint(0, len(preguntas[seccion])-1)
             if not preguntas[seccion][pregunta][0] in preguntasJugadas:
                 break
@@ -116,17 +140,22 @@ def iniciarJuego():
             print("Respuesta correcta")
             datosJugadores[turno][2] += 1
             datosPreguntasAcertadas = cargarDatos("preguntasAcertadas.dat")
-            addRegistroJugadasPorJugador(datosPreguntasAcertadas, datosJugadores[turno][0])
-            guardarPreguntaFallada("preguntasAcertadas.dat", datosPreguntasAcertadas)
+            addRegistroJugadasPorJugador(
+                datosPreguntasAcertadas, datosJugadores[turno][0])
+            guardarPreguntaFallada(
+                "preguntasAcertadas.dat", datosPreguntasAcertadas)
 
         else:
             print("Respuesta incorrecta")
             falladas = cargarDatos("preguntasFalladas.dat")
-            falladasCargadas = addPreguntasFalladas(preguntas[seccion][pregunta][0], falladas)
+            falladasCargadas = addPreguntasFalladas(
+                preguntas[seccion][pregunta][0], falladas)
             guardarPreguntaFallada("preguntasFalladas.dat", falladasCargadas)
             datosFallosPorJugador = cargarDatos("fallosPorJugador.dat")
-            addRegistroJugadasPorJugador(datosFallosPorJugador, datosJugadores[turno][0])
-            guardarPreguntaFallada("fallosPorJugador.dat", datosFallosPorJugador)
+            addRegistroJugadasPorJugador(
+                datosFallosPorJugador, datosJugadores[turno][0])
+            guardarPreguntaFallada(
+                "fallosPorJugador.dat", datosFallosPorJugador)
         turno += 1
     if tipoJuego:
         sumaEquipo1 = datosJugadores[0][2] + datosJugadores[1][2]
@@ -148,15 +177,16 @@ def iniciarJuego():
         addRegistroJugadasPorJugador(ganadores, ganador)
         guardarPreguntaFallada("ganadores.dat", ganadores)
     for i in range(0, cantJugadores):
-        #haz que se muestre la cantidad de preguntas acertadas por cada jugador. Haz que se muestre el porcentaje
-        print(f"{datosJugadores[i][0]} acertó {datosJugadores[i][2]} preguntas ({(datosJugadores[i][2]/preguntasPorJugador)*100}%)")
-    for i in range(0,len(datosJugadores)):
+        # haz que se muestre la cantidad de preguntas acertadas por cada jugador. Haz que se muestre el porcentaje
+        print(
+            f"{datosJugadores[i][0]} acertó {datosJugadores[i][2]} preguntas ({(datosJugadores[i][2]/preguntasPorJugador)*100}%)")
+    for i in range(0, len(datosJugadores)):
         datosCargados = cargarDatos("juegosDeUsuarios.dat")
-        datoActualizado = sumarJuegoAJugador(datosJugadores[i][1], datosCargados)
+        datoActualizado = sumarJuegoAJugador(
+            datosJugadores[i][1], datosCargados)
         escribirDatos("juegosDeUsuarios.dat", datoActualizado)
 
-    return 
-
+    return
 
 
 def cargarDatos(archivo):
@@ -164,6 +194,8 @@ def cargarDatos(archivo):
     preguntas = archivo.read()
     archivo.close()
     return eval(preguntas)
+
+
 def menu():
     print("""
         TRIVIAINFO
@@ -216,5 +248,6 @@ def menu():
     elif opcion == 4:
         print("Juego terminado. ")
         exit()
-menu()
 
+
+menu()
